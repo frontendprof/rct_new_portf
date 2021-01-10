ef77b97d81c30125a4a412a6df79a39a3ba29943
frontendprof <https://github.com/frontendprof>
2021-01-10 11:48:46 +0500

    Form and input in contacts component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,19 +1,66 @@
 
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles"
-import { TextArea,Typography,Box,Grid,Button} from "@material-ui/core"
+import {makeStyles,withStyles} from "@material-ui/core/styles"
+import { TextField,Typography,Box,Grid,Button} from "@material-ui/core"
 // import SendIcon from "@material-ui/icons/SendIcon"
 import Navbar from "./Navbar"
 
 
 
 
+const InputField=withStyles({
+    root:{
+        "& label.Mui-focused":{
+            color:"tomato"
+        },
+        "& label":{
+            color:"tan"
+        },
+        "& .MuiOutlinedInput-root":{
+            "& fieldset":{
+                borderColor:"tan"
+            },
+            "&:hover fieldset":{
+                borderColor:"tan"
+            }
+        }
+    }
+})(TextField);
+
+
 const Contacts = () => {
     return (
-        <div>
-            <Navbar />
-            <h2>Hello from contacts</h2>
-        </div>
+        <Box component="div">
+            <Grid container justify="center">
+                <Box component="form">
+                    <Typography>Hire or Contact me</Typography>
+                    <InputField fullWidth={true} 
+                        label="Name" 
+                        variant="outlined" 
+                        margin="dense" 
+                        size="medium"
+                        inputProps={{ style:{color:"white"} }}
+
+                    />
+                    <br />
+                    <InputField 
+                        fullWidth={true} 
+                        label="Email" 
+                        variant="outlined" 
+                        margin="dense" size="medium" 
+                        inputProps={{ style:{color:"white"} }}
+                    />
+                    <InputField 
+                        fullWidth={true} 
+                        label="Company name" 
+                        variant="outlined" 
+                        margin="dense" 
+                        size="medium" 
+                        inputProps={{ style:{color:"white"} }}
+                    />
+                </Box>
+            </Grid>
+        </Box>
     )
 }
 
