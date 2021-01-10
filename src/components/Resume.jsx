d98223a737a1a37f3e81fe50c4317462deea9d17
frontendprof <https://github.com/frontendprof>
2021-01-10 22:32:52 +0500

    Styling timeline in resume component part2

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles(theme =>({
             content:"''",
             position:"absolute",
             height:"100%",
-            border:"1px solid white",
+            border:"1px solid tan",
             right:"40px",
             top:0
         },
@@ -43,17 +43,32 @@ const useStyles = makeStyles(theme =>({
         clear:"both",
         "&:after":{
             content:"''",
-            position:"relative"
+            position:"absolute"
         },
         "&:before":{
             content:"''",
             position:"absolute",
             right:"-0.625rem",
-            top:"calc(50%-5px)",
+            top:"calc(50% - 5px)",
             borderStyle:"solid",
             borderColor:"tomato tomato transparent transparent",
             borderWidth:"0.625rem",
             transform:"rotate(45deg)"
+        },
+        [theme.breakpoints.up("md")]:{
+            width:'44%',
+            margin:"1rem",
+            "&:nth-of-type(2n)":{
+                float:"right",
+                margin:"1rem",
+                borderColor:"tan"
+                },
+            "&:nth-of-type(2n):before":{
+                right:"auto",
+                left:"-0.625rem",
+                borderColor:"transparent transparent tomato tomato"
+            }
+
         }
     },
     timeLineYear:{
@@ -80,6 +95,16 @@ const useStyles = makeStyles(theme =>({
                 }
         }
 
+    },
+    heading:{
+        color:"tomato",
+        padding:"3rem 0",
+        textTransform:"uppercase"
+    },
+    subHeading:{
+        color:"white",
+        padding:"0",
+        textTransform:"uppercase"
     }
 
 }));
@@ -93,13 +118,80 @@ const Resume = () => {
         <div>
             <Navbar />
             <Box component="header" className={classes.mainContiner}>
-                <Typography variant="h4" align="center">Working Experience</Typography>
+
+                
+                <Typography variant="h4" align="center" className={classes.heading}>Working Experience</Typography>
 
                 <Box component="div" className={classes.timeLine}>
+
+                    {/* year item 1 */}
+
+                    <Typography variant="h3" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2018-2019
+                    </Typography>
+
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>ITSM Administrator</Typography>
+                        <Typography variant="body1" align="center" style={{ color:"tomato" }}>Ripton Solutions</Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color:"tan" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam praesentium explicabo unde in earum commodi. Eligendi at repellat dolorem?</Typography>
+                    </Box>
+
+                    {/* year item 2 */}
+
                     <Typography variant="h3" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
                         2019
                     </Typography>
+
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>ITOM Developer</Typography>
+                        <Typography variant="body1" align="center" style={{ color:"tomato" }}>Ripton Solutions</Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color:"tan" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam praesentium explicabo unde in earum commodi. Eligendi at repellat dolorem?</Typography>
+                    </Box>
+
+
+                     {/* year item 3 */}
+
+                     <Typography variant="h3" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2019-2020
+                    </Typography>
+
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>Freelancer</Typography>
+                        <Typography variant="body1" align="center" style={{ color:"tomato" }}>Diffrent source</Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color:"tan" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam praesentium explicabo unde in earum commodi. Eligendi at repellat dolorem?</Typography>
+                    </Box>
+
+
+                     {/* year item 4 */}
+
+                     <Typography variant="h3" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2020
+                    </Typography>
+
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>Teacher</Typography>
+                        <Typography variant="body1" align="center" style={{ color:"tomato" }}>Zetsoft Academy</Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color:"tan" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam praesentium explicabo unde in earum commodi. Eligendi at repellat dolorem?</Typography>
+                    </Box>
+
+
+                     {/* year item 5 */}
+
+                     <Typography variant="h3" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2020
+                    </Typography>
+
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="center" className={classes.subHeading}>Frontend Developer</Typography>
+                        <Typography variant="body1" align="center" style={{ color:"tomato" }}>IT Pro Academy</Typography>
+                        <Typography variant="subtitle1" align="center" style={{ color:"tan" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam praesentium explicabo unde in earum commodi. Eligendi at repellat dolorem?</Typography>
+                    </Box>
+
+
                 </Box>
+                
+
+
             </Box>
         </div>
     )
